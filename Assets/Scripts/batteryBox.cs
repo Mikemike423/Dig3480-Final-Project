446430c60a747d21bc9793b96ccc6e9f6833e563
MikeMike423 <Michaelmagram423@gmail.com>
2022-04-23 03:29:48 -0400

    April 23, 2022 3:29 AM

--- Assets/Scripts/batteryBox.cs
+++ Assets/Scripts/batteryBox.cs
@@ -25,7 +25,7 @@ public class batteryBox : MonoBehaviour
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "froghouse")
         {
