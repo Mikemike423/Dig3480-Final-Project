3a4105f42aa2abd1a1ec4b5465fa248c8fe4973c
MikeMike423 <Michaelmagram423@gmail.com>
2022-04-22 23:41:45 -0400

    April 22, 2022 11:41 PM

--- Assets/Scripts/BossConroller.cs
+++ Assets/Scripts/BossConroller.cs
@@ -21,7 +21,7 @@ public class BossConroller : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip fixedClip;
 
-    GameObject 
+    public GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
@@ -62,7 +62,7 @@ public class BossConroller : MonoBehaviour
         {
             if(!shooting) {
                 shooting=true;
-                GameObject projectileObject = Instantiate(, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+                GameObject projectileObject = Instantiate(projectile, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
                 Projectile projectile = projectileObject.GetComponent<Projectile>();
             }
